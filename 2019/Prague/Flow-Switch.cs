3a55fb8376da8970c55f47638c43005d6032e738
Jared Parsons <jaredpparsons@gmail.com>
2019-11-02 10:40:05 -0700

    Status

---
+++ 2019/Prague/Flow-Switch.cs
@@ -0,0 +1,18 @@
+﻿#nullable enable
+using System;
+
+class Swicthy
+{
+    void PrintIt(string? str)
+    {
+        if (str == null)
+        {
+            str = "";
+        }
+
+        Console.WriteLine($"{str.Length} - {str}");
+    }
+
+}
+
+
---
+++ 2019/Prague/FrameworkAnnotations.cs
@@ -0,0 +1,23 @@
+﻿using System.IO;
+
+class PopQuiz
+{
+    void M(string path)
+    {
+//#nullable enable
+        string dirName  = Path.GetDirectoryName(path);
+        string fileName = Path.GetFileName(path);
+        string fullPath = Path.GetFullPath(path);
+        string pathRoot = Path.GetPathRoot(path);
+        string relative = Path.GetRelativePath(@"C:\Temp", path);
+#nullable disable
+
+//#nullable enable
+        Path.GetDirectoryName(null);
+        Path.GetFileName(null);
+        Path.GetFullPath(null);
+        Path.GetPathRoot(null);
+        Path.GetRelativePath(null, null);
+#nullable disable
+    }
+}
