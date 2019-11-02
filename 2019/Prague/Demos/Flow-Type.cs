2e148e27c78e85df582bb5ab09662c8b8ca9d02e
Jared Parsons <jaredpparsons@gmail.com>
2019-11-02 10:43:36 -0700

    Backing up presentation

---
+++ 2019/Prague/Demos/Flow-Type.cs
@@ -0,0 +1,16 @@
+﻿#nullable enable
+using System;
+
+class StringUtil
+{
+    void PrintIt(string? str)
+    {
+        if (str == null)
+        {
+            str = "";
+        }
+
+        Console.WriteLine($"{str.Length} - {str}");
+    }
+
+}
---
+++ 2019/Prague/Demos/Slides/Annotations-Are-Not-Enough.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+#nullable enable
+
+class Annotations_Are_Not_Enough
+{
+    class UserData { }
+
+    Dictionary<string, UserData> _userDataMap;
+
+UserData GetUserData(string? id)
+{
+    string id2;
+    if (id is null)
+    {
+        id2 = GetDefaultId();
+    }
+    else
+    {
+        // Still a warning because assigning 
+        // string? to string
+        id2 = id;
+    }
+
+    // Safe now
+    return _userDataMap[id2];
+}
+
+    string GetDefaultId()
+    {
+
+    }
+}
+}
