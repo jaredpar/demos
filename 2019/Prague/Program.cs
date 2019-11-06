7b79ccfed0f75a266846ee4adcc343f220c43e90
Jared Parsons <jaredpparsons@gmail.com>
2019-11-06 08:45:31 -0800

    Backing up presentation

--- 2019/Prague/Program.cs
+++ 2019/Prague/Program.cs
@@ -1,45 +1,10 @@
 ﻿using System;
-using System.Collections.Generic;
-
-#nullable enable
 
 class Program
 {
     static void Main(string[] args)
     {
-        string local;
-        Console.WriteLine("Hello World!");
-        var s = GetIt();
-        s ??= "";
-        s.ToLower();
-
-        List<string> ss = null;
-        List<string?> su = null;
-        List<string?>? uu = null;
-        List<string>? us = null;
-
-        us = ss;
-        su = ss!;
-        uu = su;
-        us = uu;
     }
-
-    static string? GetIt() => null;
-
-#pragma warning disable 168
-        static void Example()
-    {
-// non-nullable string
-string s1;
-
-// nullable string
-string? s2;
-
-// non-nullable List<T> which contains nullable string values
-List<string?> list;
-
-    }
-
 }
 
 
