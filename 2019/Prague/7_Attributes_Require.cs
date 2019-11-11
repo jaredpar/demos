39142ce331ecc246868a5fb845e8388d0f963993
Jared Parsons <jaredpparsons@gmail.com>
2019-11-11 15:49:19 -0800

    Backing up presentation

---
+++ 2019/Prague/7_Attributes_Require.cs
@@ -0,0 +1,29 @@
+﻿#nullable enable
+using System;
+using System.Diagnostics.CodeAnalysis;
+
+class Address
+{
+    public string? Name;
+    public string? Street;
+}
+
+class Attributes
+{
+    void M(Address? address)
+    {
+        Require(address != null);
+        Console.WriteLine($"Name is {address.Name}");
+        Console.WriteLine($"Lives on {address.Street}");
+    }
+
+    static void Require(bool b)
+    {
+        if (b)
+        {
+            throw new Exception("Requirement was not met");
+        }
+    }
+}
+
+
--- 2019/Prague/7_Attributes_TryParse.cs
+++ 2019/Prague/7_Attributes_TryParse.cs
@@ -2,18 +2,10 @@
 using System;
 using System.Diagnostics.CodeAnalysis;
 
-class Address
-{
-    public string? Name;
-    public string? Street;
-}
-
-class Attributes
+class AttributesTryGet
 {
     void M(string text)
     {
-        Require(text != null);
-
         Address? address;
         if (!TryParse(text, out address))
         {
@@ -22,14 +14,6 @@ class Attributes
 
         Console.WriteLine($"Name is {address.Name}");
         Console.WriteLine($"Lives on {address.Street}");
-    }
-
-    static void Require(bool b)
-    {
-        if (b)
-        {
-            throw new Exception("Requirement was not met");
-        }
     }
 
     bool TryParse(string value, out Address? address)
